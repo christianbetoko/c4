8f1e05fdfdee63d869835bb4d91fdacd9b3d359e
betoko <christianbetoko@gmail.com>
2026-05-26 14:40:18 +0100

    vision

--- app/Livewire/VisionPage.php
+++ app/Livewire/VisionPage.php
@@ -3,11 +3,16 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-
+use App\Models\Enterprise;
+use Carbon\Carbon;
+use Livewire\Attributes\Title;
+#[Title('Vision - C4')]
 class VisionPage extends Component
 {
     public function render()
     {
-        return view('livewire.vision-page');
+        Carbon::setLocale('fr');
+        $enterprise = Enterprise::first();
+        return view('livewire.vision-page', compact('enterprise'));
     }
 }
--- resources/views/livewire/vision-page.blade.php
+++ resources/views/livewire/vision-page.blade.php
@@ -5,7 +5,7 @@
 				<div class="row">
 				  <div class="col-lg-12 col-sm-12 col-xs-12 text-center">
 					<div class="section-top-title">
-						<h1>About us</h1>		
+						<h1>Vision</h1>		
 					</div>
 				  </div><!--- END COL -->				  
 				</div><!--- END ROW -->
@@ -19,8 +19,8 @@
 				<div class="row text-center">					
 					<div class="offset-lg-1 col-lg-10 col-sm-12 col-xs-12 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.1s" data-wow-offset="0">
 						<div class="single_about_content">
-							<h2>Prebuilt websites are designed <br /> to save you time.</h2>
-							<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+							<h2>{{$enterprise->name}}</h2>
+							{!!$enterprise->about!!}
 						</div>
 					</div><!-- END COL -->					
 				</div><!-- END ROW -->				
